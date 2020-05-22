a7b5ccd6020130727f83371075b3caf83588d094
Andrew Kishino <AndrewKishino@gmail.com>
2020-05-22 14:00:45 -0700

    Update to latest version of taquito wallet branch

--- src/app/services/operation-request-source/wallet-interaction.service.ts
+++ src/app/services/operation-request-source/wallet-interaction.service.ts
@@ -3,47 +3,44 @@ import {
   BeaconMessageType,
   OperationRequest as AirgapOperationRequest,
   PermissionRequest as AirgapPermissionRequest,
-  OperationResponse,
-  PermissionResponse,
-} from '@airgap/beacon-sdk';
-import { Injectable } from '@angular/core';
-import { defer, NEVER, Observable, ReplaySubject } from 'rxjs';
+  OperationResponseInput,
+  PermissionResponseInput,
+} from "@airgap/beacon-sdk";
+import { Injectable } from "@angular/core";
+import { defer, NEVER, Observable, ReplaySubject } from "rxjs";
 import {
   filter,
   first,
   shareReplay,
   switchMap,
   withLatestFrom,
-} from 'rxjs/operators';
-import { Network } from '../../models/network';
-import { OperationRequest } from '../../models/operation';
-import { PermissionRequest } from '../../models/permission-request';
-import { AccountService } from '../account.service';
-import { OperationFactoryService } from '../operation-factory.service';
-import { WalletInteractionStandard } from '../operation-listener.service';
+} from "rxjs/operators";
+import { OperationRequest } from "../../models/operation";
+import { PermissionRequest } from "../../models/permission-request";
+import { AccountService } from "../account.service";
+import { OperationFactoryService } from "../operation-factory.service";
+import { WalletInteractionStandard } from "../operation-listener.service";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class WalletInteractionService implements WalletInteractionStandard {
   constructor(
     private accounts: AccountService,
-    private factory: OperationFactoryService
+    private factory: OperationFactoryService,
   ) {}
 
   private paired = new ReplaySubject(1);
   public message$ = new ReplaySubject(1);
 
   private client$ = defer(async () => {
-    const client = new WalletClient({ name: 'test' });
+    const client = new WalletClient({ name: "test" });
     await client.init();
     await client.connect(async (message: AirgapPermissionRequest) => {
       if (message.type === BeaconMessageType.PermissionRequest) {
         const account = await this.accounts.getCurrentAccount();
-        const response: PermissionResponse = {
+        const response: PermissionResponseInput = {
           id: message.id,
-          version: message.version,
-          beaconId: message.beaconId,
           type: BeaconMessageType.PermissionResponse,
           pubkey: await account.getPK(),
           network: message.network,
@@ -62,38 +59,33 @@ export class WalletInteractionService implements WalletInteractionStandard {
     filter((x: any) => x.type === BeaconMessageType.OperationRequest),
     withLatestFrom(this.client$),
     switchMap(async ([x, client]: [AirgapOperationRequest, WalletClient]) => {
-      const op = await this.factory.create(x.operationDetails as any, 'test');
+      const op = await this.factory.create(x.operationDetails as any, "test");
       op.response$.subscribe(async ({ hash }) => {
-        const response: OperationResponse = {
+        const response: OperationResponseInput = {
           id: x.id,
-          version: x.version,
-          beaconId: x.beaconId,
           type: BeaconMessageType.OperationResponse,
           transactionHash: hash,
         };
         await client.respond(response);
       });
       return op;
-    })
+    }),
   );
 
   public pair(pairingStr: string) {
     return this.client$
       .pipe(
         switchMap(async (client) => {
-          // Force re-add peer
-          await client.removeAllPeers();
-          await client.addPeer({
-            name: 'test',
-            pubKey: pairingStr,
-            relayServer: 'matrix.papers.tech',
-          } as any);
+          await client.addPeer(
+            {
+              name: "test",
+              pubKey: pairingStr,
+              relayServer: "matrix.papers.tech",
+            } as any,
+          );
           this.paired.next(true);
-          // setTimeout(() => {
-          //   this.permissions$.subscribe();
-          // }, 1000);
         }),
-        first()
+        first(),
       )
       .toPromise();
   }
