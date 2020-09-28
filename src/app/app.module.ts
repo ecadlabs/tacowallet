0845c26c8436af044a48964dce3304de0987e285
apoorva konatala <akonatala@navarik.com>
2020-09-28 00:39:15 -0700

    transaction list page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { WalletInteractionService } from './services/operation-request-source/wallet-interaction.service';
-import { OperationRequestService } from './services/operation-request-source/operation-request.service';
-import { FaucetKeyModalComponent } from './faucet-key-modal/faucet-key-modal.component';
-import { FormsModule } from '@angular/forms';
-import { ConfirmModalModule } from './confirm-modal/confirm-modal.module';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { WalletInteractionService } from "./services/operation-request-source/wallet-interaction.service";
+import { OperationRequestService } from "./services/operation-request-source/operation-request.service";
+import { FaucetKeyModalComponent } from "./faucet-key-modal/faucet-key-modal.component";
+import { FormsModule } from "@angular/forms";
+import { ConfirmModalModule } from "./confirm-modal/confirm-modal.module";
+
+import { HttpClientModule } from "@angular/common/http";
+import { APOLLO_OPTIONS } from "apollo-angular";
+import { HttpLink } from "apollo-angular/http";
+import { InMemoryCache } from "@apollo/client/core";
 
 @NgModule({
   declarations: [AppComponent, FaucetKeyModalComponent],
@@ -21,14 +26,27 @@ import { ConfirmModalModule } from './confirm-modal/confirm-modal.module';
     AppRoutingModule,
     ConfirmModalModule,
     FormsModule,
+    HttpClientModule,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: 'TEST', multi: true, useExisting: WalletInteractionService },
-    { provide: 'TEST', multi: true, useExisting: OperationRequestService },
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: "TEST", multi: true, useExisting: WalletInteractionService },
+    { provide: "TEST", multi: true, useExisting: OperationRequestService },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: "https://carthagenet.idx.tez.ie/graphql",
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Tezos } from '@taquito/taquito';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { OperationRequest } from '../models/operation';
-import { AccountService } from '../services/account.service';
-import { OperationApproverService } from '../services/operation-approver.service';
-import { SummaryService } from '../apps-services/summary.service';
+import { Component, OnInit } from "@angular/core";
+import { Tezos } from "@taquito/taquito";
+import { Observable } from "rxjs";
+import { switchMap } from "rxjs/operators";
+import { OperationRequest } from "../models/operation";
+import { AccountService } from "../services/account.service";
+import { OperationApproverService } from "../services/operation-approver.service";
+import { SummaryService } from "../apps-services/summary.service";
+import { Apollo, gql } from "apollo-angular";
+import { formatDate } from "@angular/common";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: 'list.page.html',
-  styleUrls: ['list.page.scss'],
+  selector: "app-list",
+  templateUrl: "list.page.html",
+  styleUrls: ["list.page.scss"],
 })
 export class ListPage implements OnInit {
+  transactions: any[];
+  currentPKH: any;
+
+  todayDate: Date = new Date();
+
   public txList$: Observable<
     OperationRequest[]
   > = this.account.currentAccount$.pipe(
@@ -24,15 +31,31 @@ export class ListPage implements OnInit {
   constructor(
     private account: AccountService,
     private operationApprover: OperationApproverService,
-    private summaryService: SummaryService
+    private summaryService: SummaryService,
+    private apollo: Apollo
   ) {}
 
   public getStrTemplate(op: any) {
+    console.log("oop", op);
     return `${op.kind} To: ${op.destination} Amount: ${Tezos.format(
-      'mutez',
-      'tz',
+      "mutez",
+      "tz",
       op.amount
     ).toString()} tz`;
+  }
+
+  public getTransactionAmount(amount) {
+    return `${Tezos.format("mutez", "tz", amount).toString()} tz`;
+  }
+
+  public currentAccountPKH$ = this.account.currentAccount$.pipe(
+    switchMap((account) => {
+      return account.getPKH();
+    })
+  );
+
+  getFormatedDate(date) {
+    return formatDate(date, "MMM d, y, h:mm:ss a", "en-US");
   }
 
   async showSummary(op: OperationRequest) {
@@ -47,7 +70,31 @@ export class ListPage implements OnInit {
     await this.operationApprover.approve(op);
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.currentAccountPKH$.subscribe((pkh) => {
+      this.currentPKH = pkh;
+      this.apollo
+        .watchQuery({
+          query: gql`
+        {
+          account(account: "${pkh}") {
+            transactions(limit: 10,  order_by: {field: "timestamp", direction: "desc"}) {
+              source
+              destination
+              amount
+              timestamp
+              gas
+              hash
+            }
+          }
+        }
+      `,
+        })
+        .valueChanges.subscribe((result: any) => {
+          this.transactions = result?.data?.account?.transactions;
+        });
+    });
+  }
   // add back when alpha.4 is out
   // navigate(item) {
   //   this.router.navigate(['/list', JSON.stringify(item)]);
